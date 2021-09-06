95f83ac05650b34611759181f838172667ddae95
noob-coder69 <78252850+noob-coder69@users.noreply.github.com>
2021-09-07 01:42:17 +0530

    Update Ping.js

--- Commands/Ping.js
+++ Commands/Ping.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 
-module.exports.run = async (client, message, args) => {
+exports.run = async (client, message, args) => {
     message.channel.send(":timer: **|** Chargement ...").then(m =>{
         m.edit(`:ping_pong: **|** Hello, **${client.user.username}** ping is \`${m.createdTimestamp - message.createdTimestamp}ms\` !`)
       })
