26a7c9b2bf0073a60b9f0dbe50cba9da1d86c539
noob-coder69 <78252850+noob-coder69@users.noreply.github.com>
2021-09-06 22:45:09 +0530

    Update Me.js
    change me to invites

--- Commands/Me.js
+++ Commands/Me.js
@@ -11,14 +11,14 @@ exports.run = async (client, message, args) => {
     var victim = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
     var data = db.get(`invites.${victim.id}`) || { total: 0, fake: 0, inviter: null, regular: 0, bonus: 0, leave: 0 };
     var embed = new Discord.MessageEmbed()
-    .setDescription(`You have **${(data.total || 0) + (data.bonus || 0)}** invites! (**${data.regular || 0}** regular, **${data.bonus || 0}** bonus, **${data.leave || 0}** leaves, **${data.fake || 0}** fake)`)
+    .setDescription(`The user has **${(data.total || 0) + (data.bonus || 0)}** invites! (**${data.regular || 0}** regular, **${data.bonus || 0}** bonus, **${data.leave || 0}** leaves, **${data.fake || 0}** fake)`)
     .setColor("RANDOM");
     message.channel.send(embed);
 };
 
 exports.conf = {
-    commands: ["me"],
-    usage: "[p]me",
+    commands: ["invites"],
+    usage: "[p]invites",
     enabled: true,
     guildOnly: true
 };
